08675205fcf401218c9facb608353ff664d49fe4
2022101267 <2022101267@LAB2-012>
2023-08-22 20:51:44 -0300

    Adicionar arquivos de projeto.

---
+++ Aluguel de carro/Aluguel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluguel_de_carro
+{
+    public class Aluguel
+    {
+        public static double alugar(double dias, double inicial, double final)
+        {
+            return (dias * 95) + ((final - inicial) * 0.35);
+        }
+    }
+}
---
+++ Aluguel de carro/Program.cs
@@ -0,0 +1,11 @@
+﻿using Aluguel_de_carro;
+using System.Threading.Channels;
+
+Console.WriteLine("-=-=-=-=Aluguel de de carro=-=-=-=-");
+Console.WriteLine("Entre com a quantidade de dias que você deseja alugar o carro:");
+double dias = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite a quilometragem inicial:");
+double inicial = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite a quilometragem final:");
+double final = double.Parse(Console.ReadLine());
+Console.WriteLine("o valor do alguel é de {0}", Aluguel.alugar(dias, inicial, final));
